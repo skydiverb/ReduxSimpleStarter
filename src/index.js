726715b4efba7320f48b7143ae375d279dea89fd
bryan buitrago <bryanbuitrago86@gmail.com>
2017-01-20 17:17:49 -0500

    Refactored SearchBar component to be a class instead of a functional comp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
+
+const API_KEY = 'AIzaSyBMDaF8i8cI2R-2J68LQuZT9ozwJNlIZMM';
+
+
 // Create a new component. This component should produce some HTML
 const App = () => {
-  return <div>HI!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
