222a7cbbc1da715fbe5d3a6ff546de8361cd5456
bryan buitrago <bryanbuitrago86@gmail.com>
2017-01-21 13:21:49 -0500

    Handling user Events

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return (
-      <input type="text"/>
+      <input onChange={ event => console.log(event.target.value)}/>
     );
   }
 }
+
+
 
 export default SearchBar;
